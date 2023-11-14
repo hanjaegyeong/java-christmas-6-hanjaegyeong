fe8426216acaa82b0e48465bbd7e3db58d3a0156
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 15:58:24 +0900

    feat(domain): 총주문가격 계산하는 기능 추가

---
+++ src/main/java/christmas/domain/price/TotalOrderPrice.java
@@ -0,0 +1,27 @@
+package christmas.domain.price;
+
+import christmas.domain.menu.Menu;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class TotalOrderPrice {
+    private int totalOrderPrice;
+
+    // calculate가 나을까?
+    public int generateTotalOrderPrice(EnumMap<Menu, Integer> orderMenus) {
+        int calculatedTotalOrderPrice = 0;
+
+        // EnumMap을 순회하면서 각 메뉴의 가격과 수량을 곱하여 총 가격 계산
+        for (Map.Entry<Menu, Integer> menuEntry : orderMenus.entrySet()) {
+            Menu menu = menuEntry.getKey();
+            int quantity = menuEntry.getValue();
+            int menuPrice = menu.getPrice();
+
+            calculatedTotalOrderPrice += menuPrice * quantity;
+        }
+
+        this.totalOrderPrice = calculatedTotalOrderPrice;
+        return this.totalOrderPrice;
+    }
+}
