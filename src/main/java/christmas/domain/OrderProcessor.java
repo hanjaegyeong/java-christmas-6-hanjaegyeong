e8a373a40870f6e1d7b88d03a341c53e5920fd74
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 12:47:26 +0900

    feat(domain): 입력받은 주문을 메뉴, 개수로 분리해 저장하는 기능 추가
    - 개수가 제대로 된 정수값이 아닐 경우 IllegalArgumentException 발생
    - 메뉴판에 없는 메뉴 입력 시 IllegalArgumentException 발생

---
+++ src/main/java/christmas/domain/OrderProcessor.java
@@ -0,0 +1,63 @@
+package christmas.domain;
+
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuCategory;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderProcessor {
+    private static final int MAX_TOTAL_QUANTITY = 20;
+
+    private final EnumMap<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+
+    public void createOrder(String userInput) {
+        List<String> orders = splitOrders(userInput);
+
+        for (String order : orders) {
+            processOrder(order);
+        }
+    }
+
+    private List<String> splitOrders(String userInput) {
+        return Arrays.asList(userInput.split(","));
+    }
+
+    private void processOrder(String order) {
+        List<String> menuAndQuantity = splitMenuAndQuantity(order);
+        String menuName = extractMenuName(menuAndQuantity);
+        int quantity = extractQuantity(menuAndQuantity);
+
+        Menu menu = findMenuByName(menuName);
+
+        orderMenus.put(menu, quantity);
+    }
+
+    private List<String> splitMenuAndQuantity(String order) {
+        return Arrays.asList(order.split("-"));
+    }
+
+    private String extractMenuName(List<String> menuAndQuantity) {
+        return menuAndQuantity.get(0).trim();
+    }
+
+    private int extractQuantity(List<String> menuAndQuantity) {
+        String quantityString = menuAndQuantity.get(1).trim();
+        try {
+            return Integer.parseInt(quantityString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private Menu findMenuByName(String menuName) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equals(menuName)) {
+                return menu;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+}
