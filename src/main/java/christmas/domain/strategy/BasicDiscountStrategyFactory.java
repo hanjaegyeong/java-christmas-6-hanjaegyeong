a1cddf650eb99bd22ba6963208284540a43035f1
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 18:08:19 +0900

    refactor(domain): BasicDiscountStrategyFactory 코드 정돈

--- src/main/java/christmas/domain/strategy/BasicDiscountStrategyFactory.java
+++ src/main/java/christmas/domain/strategy/BasicDiscountStrategyFactory.java
@@ -6,8 +6,8 @@ import christmas.domain.OrderProcessor;
 import static christmas.domain.ReservationDayProcesser.isWeekend;
 
 public class BasicDiscountStrategyFactory {
-    public static BasicDiscountStrategy createDiscountStrategy(int day, OrderProcessor orderProcessor, DiscountEvents discountEvents) {
-        if (isWeekend(day)) {
+    public static BasicDiscountStrategy createDiscountStrategy(int reservationDay, OrderProcessor orderProcessor, DiscountEvents discountEvents) {
+        if (isWeekend(reservationDay)) {
             return new WeekendDiscountStrategy(orderProcessor, discountEvents);
         }
         return new WeekdayDiscountStrategy(orderProcessor, discountEvents);
