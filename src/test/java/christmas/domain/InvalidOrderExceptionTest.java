2ab4318830ec8d3ab750006b8630a31ab34404a0
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-15 00:23:16 +0900

    feat(test): InvalidOrderExceptionTest 추가

---
+++ src/test/java/christmas/domain/InvalidOrderExceptionTest.java
@@ -0,0 +1,17 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InvalidOrderExceptionTest {
+
+    @Test
+    @DisplayName("InvalidOrderException 생성 테스트")
+    public void testCreateInvalidOrderException() {
+        String errorMessage = "유효하지 않은 주문입니다.";
+        InvalidOrderException exception = new InvalidOrderException(errorMessage);
+
+        assertEquals("[ERROR] " + errorMessage, exception.getMessage());
+    }
+}
