ee9c327bac83660631bac111a190d55724c7e380
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 19:16:40 +0900

    refactor(domain): EventBadge 매서드명, 변수명 변경

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -3,30 +3,30 @@ package christmas.domain;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static christmas.utils.constants.NOTHING;
+
 public enum EventBadge {
     STAR("별", 5_000),
     TREE("트리", 10_000),
     SANTA("산타", 20_000);
 
     private final String badgeName;
-    private final int discountPrice;
+    private final int thresholdAmount;
 
-    EventBadge(String badgeName, int discountPrice) {
+    EventBadge(String badgeName, int discountAmount) {
         this.badgeName = badgeName;
-        this.discountPrice = discountPrice;
+        this.thresholdAmount = discountAmount;
     }
 
-    // 가격에 해당하는 이벤트 배지 찾기
-    // 혜택은 benefit?
-    public static String findBadgeByPrice(int totalDiscountPrice) {
+    public static String findBadgeByTotalAppliedEventsAmount(int totalAppliedEventsAmount) {
         EventBadge eventBadge = Arrays.stream(values())
-                .filter(badge -> badge.discountPrice <= totalDiscountPrice)
-                .max(Comparator.comparingInt(badge -> badge.discountPrice))
+                .filter(badge -> badge.thresholdAmount <= totalAppliedEventsAmount)
+                .max(Comparator.comparingInt(badge -> badge.thresholdAmount))
                 .orElse(null);
 
         if (eventBadge != null) {
             return eventBadge.badgeName;
         }
-        return "없음";
+        return NOTHING;
     }
 }
