1f77a6cb5522039c3730bfbe2810f829dc653868
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 12:17:12 +0900

    feat(view): 주문할 메뉴와 개수 입력 받는 기능 추가
    inputOrder()에서 입력받은 후 문자열을 그대로 리턴하도록 세팅하였다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -25,4 +25,11 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
+
+    public static String inputOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = readLine();
+        System.out.print(LINE_SEPARATOR);
+        return input;
+    }
 }
