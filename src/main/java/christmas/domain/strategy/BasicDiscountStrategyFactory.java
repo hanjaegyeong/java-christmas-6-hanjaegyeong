8950b45f0e22ec6987b4bb3efe1fa330fe19ecdd
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 20:35:32 +0900

    refactor(domain): BasicDiscountStrategyFactory에서도 ReservationDayProcessor 이름 변경

--- src/main/java/christmas/domain/strategy/BasicDiscountStrategyFactory.java
+++ src/main/java/christmas/domain/strategy/BasicDiscountStrategyFactory.java
@@ -3,7 +3,7 @@ package christmas.domain.strategy;
 import christmas.domain.DiscountEvents;
 import christmas.domain.OrderProcessor;
 
-import static christmas.domain.ReservationDayProcesser.isWeekend;
+import static christmas.domain.ReservationDayProcessor.isWeekend;
 
 public class BasicDiscountStrategyFactory {
     public static BasicDiscountStrategy createDiscountStrategy(int reservationDay, OrderProcessor orderProcessor, DiscountEvents discountEvents) {
