5049b185a27f49a7d6e254a966a3eb30811a90b7
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 20:19:27 +0900

    refactor(domain): ReservationDayProcessor 클래스명 변경 및 상수화

--- src/main/java/christmas/domain/ReservationDayProcessor.java
+++ src/main/java/christmas/domain/ReservationDayProcessor.java
@@ -4,22 +4,24 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class ReservationDayProcesser {
-    public static void validateDay(int reservationDay) throws IllegalArgumentException {
+public class ReservationDayProcessor {
+    private static final String INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    public static void validateDay(int reservationDay) {
         if (reservationDay < 1 || reservationDay > 31) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_DATE_MESSAGE);
         }
     }
 
     public static boolean isWeekend(int reservationDay) {
         LocalDate reservationDate = parseDate(reservationDay);
-        DayOfWeek dayOfWeek = reservationDate.getDayOfWeek(); // 특정 날짜의 요일 가져오는 매서드
+        DayOfWeek dayOfWeek = reservationDate.getDayOfWeek();
 
         return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
     }
 
     private static LocalDate parseDate(int reservationDay) {
-        String date = String.format("202312%02d", reservationDay); // %02d는 입력받은 day를 두 자리 수로 만들어주는 포매터
+        String date = String.format("202312%02d", reservationDay);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         return LocalDate.parse(date, formatter);
     }
