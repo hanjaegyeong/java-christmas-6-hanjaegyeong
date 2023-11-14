c277df84a73cda4652280a00c410301847cfdc14
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 15:37:18 +0900

    feat(domain): 예약일 입력받아 주말 판단하는 isWeekend() 매서드 추가
    - 실제 날짜 기반이므로 java.time 라이브러리 이용

--- src/main/java/christmas/domain/ReservationDayProcesser.java
+++ src/main/java/christmas/domain/ReservationDayProcesser.java
@@ -1,9 +1,26 @@
 package christmas.domain;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class ReservationDayProcesser {
     public static void validateDay(int reservationDay) throws IllegalArgumentException {
         if (reservationDay < 1 || reservationDay > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public static boolean isWeekend(int reservationDay) {
+        LocalDate reservationDate = parseDate(reservationDay);
+        DayOfWeek dayOfWeek = reservationDate.getDayOfWeek(); // 특정 날짜의 요일 가져오는 매서드
+
+        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
+    }
+
+    private static LocalDate parseDate(int reservationDay) {
+        String date = String.format("202312%02d", reservationDay); // %02d는 입력받은 day를 두 자리 수로 만들어주는 포매터
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        return LocalDate.parse(date, formatter);
     }
 }
