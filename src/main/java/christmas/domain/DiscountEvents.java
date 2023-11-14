aa31c09c2ff537c1fb31d6770269b534fe092844
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-14 15:33:46 +0900

    feat(domain): DiscountEvents 클래스에 총할인금액 변수 추가
    - totalDiscountPrice 변수에 총 할인금액 저장
    - 증정이벤트를 제외한 이벤트 적용될 시 총 할인금액에 덧셈

--- src/main/java/christmas/domain/DiscountEvents.java
+++ src/main/java/christmas/domain/DiscountEvents.java
@@ -7,9 +7,21 @@ import java.util.Objects;
 // 혜택 내역 + 총 할인 금액
 public class DiscountEvents {
     private final Map<String, Integer> discountEvents = new HashMap<>();
+    private int totalDiscountPrice = 0; // 총 할인 금액 래핑 고민하기***
+
+    public int getTotalDiscountPrice() {
+        return totalDiscountPrice;
+    }
 
     public void addDiscountEvent(String eventName, int discountAmount) {
         // 할인 이벤트를 Map에 추가
         discountEvents.put(eventName, discountAmount);
+        if (!Objects.equals(eventName, "증정 이벤트")) {
+            totalDiscountPrice += discountAmount;
+        }
+    }
+
+    public int calculateTotalDiscountPrice() {
+        return discountEvents.values().stream().mapToInt(Integer::intValue).sum();
     }
 }
