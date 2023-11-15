d64f289f045045ff6a35e27f9671c17722f2ddb3
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-15 16:12:59 +0900

    feat(test): GiftEventTest 코드 수정

--- src/test/java/christmas/domain/GiftEventTest.java
+++ src/test/java/christmas/domain/GiftEventTest.java
@@ -1,35 +1,33 @@
 package christmas.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GiftEventTest {
-    private DiscountEvents discountEvents;
-
-    @BeforeEach
-    public void setUp() {
-        discountEvents = new DiscountEvents();
-    }
-
     @Test
-    @DisplayName("총 적용 이벤트 금액이 임계값을 초과하는 경우 증정 이벤트를 생성하고 반환해야 한다.")
+    @DisplayName("임계값 이상일 경우 증정 이벤트가 적용되어야 한다.")
     public void testGenerateGiftEventOutput_ThresholdExceeded() {
-        int totalAppliedEventsAmount = 150_000;
-        String result = GiftEvent.generateGiftEventOutput(totalAppliedEventsAmount, discountEvents);
+        DiscountEvents mockDiscountEvents = mock(DiscountEvents.class);
+        int totalAppliedEventsAmount = 130_000;
 
+        String result = GiftEvent.generateGiftEventOutput(totalAppliedEventsAmount, mockDiscountEvents);
+
+        verify(mockDiscountEvents, times(1)).addDiscountEvent("증정 이벤트", 25_000);
         assertEquals("샴페인 1개", result);
-        assertEquals(25_000, discountEvents.getTotalDiscountAmount());
     }
 
     @Test
-    @DisplayName("총 적용 이벤트 금액이 임계값을 초과하지 않는 경우 없음을  않아야 한다.")
-    public void testGenerateGiftEventOutput_ThresholdNotExceeded() {
-        int totalAppliedEventsAmount = 10_000;
-        String result = GiftEvent.generateGiftEventOutput(totalAppliedEventsAmount, discountEvents);
+    @DisplayName("임계값 미만일 경우 증정 이벤트가 적용되지 않아야 한다.")
+    public void testGenerateGiftEventOutput_BelowThreshold() {
+        DiscountEvents mockDiscountEvents = mock(DiscountEvents.class);
+        int totalAppliedEventsAmount = 100_000;
 
-        assertEquals("없음", result);
-        assertEquals(0, discountEvents.getTotalDiscountAmount());
+        String result = GiftEvent.generateGiftEventOutput(totalAppliedEventsAmount, mockDiscountEvents);
+
+        verify(mockDiscountEvents, never()).addDiscountEvent(anyString(), anyInt());
+        assertEquals(christmas.utils.constants.NOTHING, result);
     }
 }
