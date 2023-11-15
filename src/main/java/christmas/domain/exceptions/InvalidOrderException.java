6e0f06074de6bdb0b079d6b4dcafd7edf71b1d89
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-15 18:58:55 +0900

    refactor(domain): LINE_SEPARATOR 상수화

--- src/main/java/christmas/domain/exceptions/InvalidOrderException.java
+++ src/main/java/christmas/domain/exceptions/InvalidOrderException.java
@@ -1,7 +1,9 @@
 package christmas.domain.exceptions;
 
+import static christmas.utils.Constants.LINE_SEPARATOR;
+
 public class InvalidOrderException extends IllegalArgumentException {
     public InvalidOrderException(String message) {
-        super("[ERROR] " + message);
+        super("[ERROR] " + message + LINE_SEPARATOR);
     }
 }
