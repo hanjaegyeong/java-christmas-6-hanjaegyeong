7ddd51d9d282ea42cbf5977162dc70fbc0d4a6ea
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-15 17:41:11 +0900

    refactor(domain): constants 상수화
    - 빠트린 final 붙이기
    - LINE_SEPARATOR 추가

--- src/main/java/christmas/utils/constants.java
+++ src/main/java/christmas/utils/constants.java
@@ -1,6 +1,7 @@
 package christmas.utils;
 
 public class constants {
-    public static String NOTHING = "없음";
-    public static String WON = "원";
+    public static final String NOTHING = "없음";
+    public static final String WON = "원";
+    public static final String LINE_SEPARATOR = System.lineSeparator();
 }
