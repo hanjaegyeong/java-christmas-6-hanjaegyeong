83da229177aee278af231ac99e2f13677d564ba4
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-15 15:57:05 +0900

    feat(test): ChristmasDDayDiscountDecoratorTest 추가

---
+++ src/test/java/christmas/domain/decorator/ChristmasDDayDiscountDecoratorTest.java
@@ -0,0 +1,25 @@
+package christmas.domain.decorator;
+
+import christmas.domain.DiscountEvents;
+import christmas.domain.strategy.BasicDiscountStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ChristmasDDayDiscountDecoratorTest {
+    @Test
+    @DisplayName("크리스마스 디데이 할인이 정상적으로 적용되어야 한다.")
+    public void testApplyDiscount() {
+        BasicDiscountStrategy mockStrategy = mock(BasicDiscountStrategy.class);
+        DiscountEvents discountEvents = new DiscountEvents();
+        int reservationDay = 23;
+        ChristmasDDayDiscountDecorator decorator = new ChristmasDDayDiscountDecorator(mockStrategy, reservationDay, discountEvents);
+
+        decorator.applyDiscount();
+
+        int expectedDiscount = 3200;
+        assertEquals(expectedDiscount, discountEvents.getTotalDiscountAmount());
+    }
+}
